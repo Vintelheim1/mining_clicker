eae8e2bc70df65da6ffcc7ccabb6b00d52a474d2
Vintelheim1 <daniels.bramanis09@gmail.com>
2026-05-27 16:32:31 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const dwarfText = document.getElementById("dwarfText")
-
-upgradeBtn.addEventListener("click", () => {
-  if (gold >= cost) {
-    dwarfText.innerText = "A fine upgrade! Keep mining! ⛏️"
-  } else {
-    dwarfText.innerText = "Ye need more gold, lad! 💰"
-  }
-})
